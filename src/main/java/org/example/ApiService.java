b22d7d4da698f68672ee24037329e27d1df1f924
yevgeny-sh <yevgst7@gmail.com>
2026-09-02 15:03:07 +0300

    Add render config model and parsing

--- src/main/java/org/example/ApiService.java
+++ src/main/java/org/example/ApiService.java
@@ -1,20 +1,33 @@
 package org.example;
 
-
 import kong.unirest.core.HttpResponse;
 import kong.unirest.core.Unirest;
-
+import org.json.JSONObject;
 
 public class ApiService {
 
     private static final String CONFIG_URL =
             "https://shaitest-production-3066.up.railway.app/fm1/get-render-config";
 
-    public String getRenderConfig() {
+    public RenderConfig getRenderConfig() {
 
         HttpResponse<String> response = Unirest.get(CONFIG_URL)
                 .asString();
 
-        return response.getBody();
+        JSONObject json = new JSONObject(response.getBody());
+
+        String wallCellColor = json.getString("wallCellColor");
+        String pathColor = json.getString("pathColor");
+        boolean drawGrid = json.getBoolean("drawGrid");
+        String gridColor = json.getString("gridColor");
+        int animationDelayMs = json.getInt("animationDelayMs");
+
+        return new RenderConfig(
+                wallCellColor,
+                pathColor,
+                drawGrid,
+                gridColor,
+                animationDelayMs
+        );
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,8 +6,12 @@ public class Main {
 
         ApiService apiService = new ApiService();
 
-        String config = apiService.getRenderConfig();
+        RenderConfig config = apiService.getRenderConfig();
 
-        System.out.println(config);
+        System.out.println("Wall color: " + config.getWallCellColor());
+        System.out.println("Path color: " + config.getPathColor());
+        System.out.println("Draw grid: " + config.isDrawGrid());
+        System.out.println("Grid color: " + config.getGridColor());
+        System.out.println("Animation delay: " + config.getAnimationDelayMs());
     }
 }
